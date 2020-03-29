60df408e0d7e86d6186f9ed2931c877b381569a3
usermine12 <mikaeljarv@gmail.com>
2020-03-30 01:11:21 +0300

    osa3 3.6

--- index.js
+++ index.js
@@ -37,6 +37,10 @@ app.post('/api/persons', (request, response) => {
         return response.status(400).json({
             error: 'name or number missing'
         })
+    } else if(persons.find(person => person.name === body.name)){
+        return response.status(400).json({
+            error: 'name must be unique'
+        })
     }
     
     const person = {
