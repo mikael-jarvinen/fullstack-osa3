1830fe65a6634d7097e94ec55af7b8ede2583ea0
usermine12 <mikaeljarv@gmail.com>
2020-03-30 19:58:25 +0300

    uibuild

---
+++ build/precache-manifest.f1e9725ed64a341383b303265f27874f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c365a71d2fc9f17ce34e3639f06f90a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ea0353144fcfad6d5aa",
+    "url": "/static/css/main.7efb19ac.chunk.css"
+  },
+  {
+    "revision": "59ef71189ecc327db302",
+    "url": "/static/js/2.eaf45e00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.eaf45e00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ea0353144fcfad6d5aa",
+    "url": "/static/js/main.cee22ed1.chunk.js"
+  },
+  {
+    "revision": "14c670b69b7f17d9de8b",
+    "url": "/static/js/runtime-main.ad756d60.js"
+  }
+]);
