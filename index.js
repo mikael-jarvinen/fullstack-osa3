7d7332ab52d09542354f6d33dd17855d3bc1d8de
usermine12 <mikaeljarv@gmail.com>
2020-03-31 23:52:09 +0300

    osa3 3.13

--- index.js
+++ index.js
@@ -16,26 +16,15 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
 
 app.post('/api/persons', (request, response) => {
     const body = request.body
-
-    if(!body.name || !body.number){
-        return response.status(400).json({
-            error: 'name or number missing'
-        })
-    } else if(persons.find(person => person.name === body.name)){
-        return response.status(400).json({
-            error: 'name must be unique'
-        })
-    }
     
-    const person = {
+    const person = new Person({
         name: body.name,
         number: body.number,
-        id: generateId()
-    }
+    })
 
-    persons = persons.concat(person)
-
-    response.json(person)
+    person.save().then(savedPerson => {
+        response.json(savedPerson)
+    })
 })
 
 app.get('/api/persons', (request, response) => {
@@ -45,14 +34,9 @@ app.get('/api/persons', (request, response) => {
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-
-    if(person){
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
+    Person.findById(request.params.id).then(person => {
+        response.json(person.toJSON())
+    })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
