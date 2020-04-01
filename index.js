f27f168c95ae69c70753fe9850ac63f5e53ee5fc
usermine12 <mikaeljarv@gmail.com>
2020-04-01 12:01:39 +0300

    osa3 3.16

--- index.js
+++ index.js
@@ -33,18 +33,40 @@ app.get('/api/persons', (request, response) => {
     })
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
     Person.findById(request.params.id).then(person => {
-        response.json(person.toJSON())
+        if(person){
+            response.json(person.toJSON())
+        } else {
+            response.status(404).end()
+        }
     })
+    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-
-    response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndRemove(request.params.id)
+    .then(person => {
+        response.status(204).end()
+    })
+    .catch(error => next(error))
 })
+
+const unknownEndpoint = ((request, response) => {
+    response.status(404).send({error: 'unknown endpoint'})
+})
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.log(error)
+
+    if(error.name === 'CastError'){
+        return response.status(400).send({error: 'malformatted id'})
+    }
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
